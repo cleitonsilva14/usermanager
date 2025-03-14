0f4356b743b060ceddcb927deac77a131c312437
cleitonsilva14 <cleiton18ads@gmail.com>
2025-03-14 19:20:09 -0300

    Criando PATCH mapping para atualizar senha

--- src/main/java/io/dev/usermanager/controller/UserController.java
+++ src/main/java/io/dev/usermanager/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -97,8 +98,8 @@ public class UserController {
 
     }
     
-    @PutMapping
-    public ResponseEntity<?> updateUser(@RequestParam String username, @RequestBody UserModel userModel){
+    @PatchMapping("/password")
+    public ResponseEntity<?> updateUser(@RequestParam String username, @RequestBody String newPassword){
         
         Optional<UserModel> userOptional = userService.findByUsername(username);
 
@@ -110,7 +111,7 @@ public class UserController {
 
         UserModel user = userOptional.get();
 
-        user.setPassword(userModel.getPassword());
+        user.setPassword(newPassword);
 
         userService.save(user);
 
